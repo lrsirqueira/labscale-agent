1c88acb7ad4f2717089c4d7470e6f456449b26d0
lrsirqueira <67562978+lrsirqueira@users.noreply.github.com>
2025-02-25 20:47:02 -0300

    addind route and eth1 config

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verifica se o usuário é root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Este script precisa ser executado como root!"
+    exit 1
+fi
+
 # Solicita o Network ID ao usuário
 read -p "Digite o ZeroTier Network ID: " NETWORK_ID
 
@@ -21,3 +27,29 @@ sudo zerotier-cli join "$NETWORK_ID"
 
 echo "Join solicitado! Autorize o dispositivo no painel do ZeroTier."
 echo "Para verificar o status da conexão, use: sudo zerotier-cli listnetworks"
+
+
+# Caminho do arquivo Netplan
+NETPLAN_FILE="/etc/netplan/01-netcfg.yaml"
+
+echo "Configurando a interface eth1 com IP 192.168.0.1/30..."
+cat <<EOF > $NETPLAN_FILE
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true
+    eth1:
+      addresses:
+        - 192.168.0.1/30
+      routes:
+        - to: 100.64.0.0/16
+          via: 192.168.0.2
+EOF
+
+echo "Aplicando as configurações de rede..."
+sudo netplan apply
+
+echo "Configuração concluída com sucesso!"
+
